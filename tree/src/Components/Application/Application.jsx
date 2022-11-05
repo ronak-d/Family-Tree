e9b88dd8a604383715e73f466341eaa541bd19f2
ronak-d <ronakdodeja73@gamil.com>
2022-11-05 10:28:28 +0530

    made css with MUI

--- tree/src/Components/Application/Application.jsx
+++ tree/src/Components/Application/Application.jsx
@@ -1,12 +1,52 @@
 import React from 'react'
-import styles from '../CSS/Application.module.css'
+// import styles from '../CSS/Application.module.css'
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Application = () => {
   return (
-    // body
-    <div> 
-      Application
-    </div>
+  <Box sx={{ width: "100%",display:"flex",border: "2px solid red" }}>
+
+  <Box sx={{ width: "40%",border: "2px solid red" }}>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="stretch"
+      spacing={2}
+    >
+
+      <Item>Item 1</Item>
+      <Item>Item 2</Item>
+      <Item>Item 3</Item>
+    </Stack>
+  </Box>
+
+  <Box sx={{ width: "60%",border: "2px solid red" }}>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="stretch"
+      spacing={2}
+    >
+
+      <Item>Item 1</Item>
+      <Item>Item 2</Item>
+      <Item>Item 3</Item>
+    </Stack>
+  </Box>
+  </Box>
   )
 }
 
