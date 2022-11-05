5a52b04c88e8844a748b948c7cfc2f7173adc3dd
ronak-d <ronakdodeja73@gamil.com>
2022-11-05 11:41:32 +0530

    consoled the data

--- tree/src/Components/Application/Application.jsx
+++ tree/src/Components/Application/Application.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import styles from '../CSS/Application.module.css'
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+import handleAxios from '../HandleAxios/HandleAxios';
 
 
 
@@ -16,37 +18,60 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const Application = () => {
+
+  const [addMem, setAddMem] = useState("");
+  const [data,setData] = useState(null);
+  const [loadings,setLoadings] = useState(true);
+
+  useEffect(() =>{
+    handleAxios("Family","get",data)
+    .then((res)=>{
+      setLoadings(false);
+      console.log(res.data);
+      console.log(res.data[0].familyMember[0].name);
+    })
+    .catch((err)=>{
+      console.log(err);
+      setLoadings(false);
+    });
+  },[])
+
   return (
+<>
+    <Typography variant="h6" component="h2" textAlign={"center"}>
+      Family Tree
+    </Typography>
+    
   <Box sx={{ width: "100%",display:"flex",border: "2px solid red" }}>
+    
+    <Box sx={{ width: "40%",border: "2px solid red" }}>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="stretch"
+        spacing={2}
+      >
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+    </Box>
 
-  <Box sx={{ width: "40%",border: "2px solid red" }}>
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="stretch"
-      spacing={2}
-    >
+    <Box sx={{ width: "60%",border: "2px solid red" }}>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="stretch"
+        spacing={2}
+      >
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+    </Box>
 
-      <Item>Item 1</Item>
-      <Item>Item 2</Item>
-      <Item>Item 3</Item>
-    </Stack>
   </Box>
-
-  <Box sx={{ width: "60%",border: "2px solid red" }}>
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="stretch"
-      spacing={2}
-    >
-
-      <Item>Item 1</Item>
-      <Item>Item 2</Item>
-      <Item>Item 3</Item>
-    </Stack>
-  </Box>
-  </Box>
+</>
   )
 }
 
